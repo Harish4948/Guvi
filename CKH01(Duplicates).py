01381904102c8260e2673418bd91fbfb908a6bb9
Harish4948 <36042547+Harish4948@users.noreply.github.com>
2019-04-12 12:22:52 +0530

    Add files via upload

---
+++ CKH01(Duplicates).py
@@ -0,0 +1,17 @@
+#from __future__ import print_function
+m=raw_input()
+ip=map(int,raw_input().split())
+d=dict()
+dup=list()
+for i in  ip:
+      if i not in d.keys():
+            d.update({i:1})
+      else:
+            dup.append(i)
+un=list()
+for i in dup:
+      if i not in un:
+            un.append(i)
+for i in un:
+      print(i),
+
