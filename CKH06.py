e718ab9531a5c6f0948448f68e6feff75c80512e
Harish4948 <36042547+Harish4948@users.noreply.github.com>
2019-04-12 16:26:05 +0530

    Update CKH06.py

--- CKH06.py
+++ CKH06.py
@@ -11,15 +11,4 @@ for i in l:
             print(i)
             break
       
-            #update the occurance
-##            
-##            k=d[i]
-##            d[i]=k+1
-##un=list() # To store unique elements with Occurane 1
-##for i in l:
-##      if(d[i]==1):
-##            un.append(i)
-##for i in un:
-##      print(i),
-##            
-##            
+
