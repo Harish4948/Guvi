a02d22ebbe064b2d1ecf92a4d0c47c9b9876dc8b
Harish4948 <36042547+Harish4948@users.noreply.github.com>
2019-04-12 16:24:57 +0530

    Add files via upload

--- CKH06.py
+++ CKH06.py
@@ -1,9 +1,25 @@
-n=raw_input()
+n=int(raw_input())
 l=map(int,raw_input().split()) #get the numbers
 d=dict()    # store the occurances
+
 for i in l:
       if i not in d.keys(): #unique elements
             d.update({i:1})
+            if(l.index(i)==n-1):
+                  print("unique")
       else:
             print(i)
             break
+      
+            #update the occurance
+##            
+##            k=d[i]
+##            d[i]=k+1
+##un=list() # To store unique elements with Occurane 1
+##for i in l:
+##      if(d[i]==1):
+##            un.append(i)
+##for i in un:
+##      print(i),
+##            
+##            
