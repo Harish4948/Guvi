416fc4d6fdbfb0afe2b3f09d60aa4f14eece340f
Harish4948 <36042547+Harish4948@users.noreply.github.com>
2019-04-12 11:00:13 +0530

    Update CKPL03.py

--- CKPL03.py
+++ CKPL03.py
@@ -1,9 +1,9 @@
 #COde to reverse a number
 n=int(raw_input())
 if(n>=0):
-  n=str(n)
-  print(n[::-1])
+    n=str(n)
+    print(n[::-1])
 else:
-  n=str(n)
-  n=[1:-1]+n[-1]
-  print("-"+n[::-1])
+    n=str(n)
+    n=n[1:-1]+n[-1]
+    print("-"+n[::-1])
