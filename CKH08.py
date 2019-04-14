8612063921ab722f4501c0a1f35baf5e2002cb5b
Harish4948 <36042547+Harish4948@users.noreply.github.com>
2019-04-14 21:17:33 +0530

    Add files via upload

---
+++ CKH08.py
@@ -0,0 +1,25 @@
+n=raw_input()
+l=map(int,raw_input().split())
+op=list()
+for i,v in enumerate(l):
+      for j in range(i+1,len(l)):
+            for k in range(j+1,len(l)):
+                  temp=[l[i],l[j],l[k]]
+                  if(temp[0]+temp[1]==temp[2]):
+                        
+                        op.append(temp)
+for i in range(len(op)):
+      for j in range(3):
+            print(op[i][j]),
+      print
+
+
+      
+##      if(i+3<=len(l)):
+##            temp=l[i:i+3]
+##            print(temp)
+##            if(len(temp)==3):
+##                  if(temp[0]+temp[1]==temp[2]):
+##                        op.append(temp)
+##print(op)
+##                        
