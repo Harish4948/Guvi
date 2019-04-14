c260f2a197db0403c27c8e6b7d9dbd38f34b4c77
Harish4948 <36042547+Harish4948@users.noreply.github.com>
2019-04-14 21:54:19 +0530

    Add files via upload

---
+++ CKH09.py
@@ -0,0 +1,21 @@
+import sys
+n=raw_input()
+l=map(int,raw_input().split())
+s=abs(l[0]+l[1])
+if s==0:
+      print(l[0]),
+      print(l[1])
+      sys.exit()
+for i,v in enumerate(l):
+      for j in range(i+1,len(l)):
+            temp=[l[i],l[j]]
+            res=abs(temp[0]+temp[1])
+            if res<=s:
+                  s=res
+                  x,y=l[i],l[j]
+                  if s==0:
+                        print(x),
+                        print(y)
+                        sys.exit()
+print(x),
+print(y)
