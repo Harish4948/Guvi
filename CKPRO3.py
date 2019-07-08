b4c02d0296894fcab10ec6cc2ce44d209445887b
Harish4948 <36042547+Harish4948@users.noreply.github.com>
2019-07-08 21:17:15 +0530

    Update CKPRO3.py

--- CKPRO3.py
+++ CKPRO3.py
@@ -5,8 +5,21 @@ if  l2>l1:
     while(i<l1 and x[i]==y[i]):
         i+=1
     print(l2-i)
-elif(l2==l2 or l1>l2):
+elif(l2==l1):
     i=0
     while i<l2 and x[i]==y[i]:
         i+=1
     print(l1-i)
+else:
+    i=0
+    while i<l2 and x[i]==y[i]:
+        i+=1
+    s31 = x[i:]
+    s32 = y[i:]
+    L = list(s31)
+    k = 0
+    for c in s32 :
+        if c in L :
+            k += 1
+            L.remove(c)
+    print(l1-i-k)
