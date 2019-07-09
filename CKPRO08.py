f8b65008c661a9818b835f4f45c4fdf3f2e189ec
Harish4948 <36042547+Harish4948@users.noreply.github.com>
2019-07-09 08:41:45 +0530

    Add files via upload

---
+++ CKPRO08.py
@@ -0,0 +1,12 @@
+from fractions import gcd
+n,q=map(int,raw_input().split())
+arr=map(int,raw_input().split())
+res=list()
+for i in range(q):
+      q=map(int,raw_input().split())
+      if(arr[q[0]-1]==arr[q[1]-1]):
+            res.append(arr[q[0]-1])
+      else:
+            res.append(gcd(arr[q[0]-1],arr[q[1]-1]))
+for i in res:
+      print(i)
